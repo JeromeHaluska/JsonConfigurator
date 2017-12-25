e445100f575f6ec7738b1f382b69ff0c1986c00f
Stoaser <jerome.haluska@gmx.de>
2017-12-25 19:27:21 +0100

    Change main project name

--- JH.JsonConfigurator/App.xaml.cs
+++ JH.JsonConfigurator/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace JH.JsonConfigurator
+namespace JH.JsonConfiguratorView
 {
     /// <summary>
     /// Interaction logic for App.xaml
